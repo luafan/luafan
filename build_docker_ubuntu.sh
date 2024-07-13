9617308dd6498317aaa431f7d05089c61946f235
Sam <sam.chang@me.com>
2024-07-13 17:45:19 +0800

    fix circle ci

--- build_docker_ubuntu.sh
+++ build_docker_ubuntu.sh
@@ -11,7 +11,7 @@ apt update \
     && wget http://luarocks.org/releases/luarocks-$LUAROCKS_VERSION.tar.gz && tar xzf luarocks-$LUAROCKS_VERSION.tar.gz && cd luarocks-$LUAROCKS_VERSION && ./configure && make build && make install && cd .. \
     && wget https://github.com/MariaDB/server/archive/mariadb-$MARIADB_VERSION.tar.gz && tar xzf mariadb-$MARIADB_VERSION.tar.gz && cd server-mariadb-$MARIADB_VERSION && cmake . && cd libmysql && make install && cd ../include && make install && cd ../.. && rm -rf mariadb-$MARIADB_VERSION.tar.gz server-mariadb-$MARIADB_VERSION \
     && wget https://www.openssl.org/source/openssl-$OPENSSL_VERSION.tar.gz && tar xzf openssl-$OPENSSL_VERSION.tar.gz && cd openssl-$OPENSSL_VERSION && ./config && make && make install && cd .. \
-    && luarocks make luafan-$LUAFAN_VERSION.rockspec MARIADB_VERSION_DIR=/usr/local/mysql CURL_INCDIR=/usr/include/`uname -m`-linux-gnu \
+    && luarocks make luafan-$LUAFAN_VERSION.rockspec MARIADB_DIR=/usr/local/mysql CURL_INCDIR=/usr/include/`uname -m`-linux-gnu \
     && luarocks install compat53 && luarocks install lpeg && luarocks install lua-cjson 2.1.0-1 && luarocks install luafilesystem \
     && luarocks install lzlib && luarocks install openssl && luarocks install lbase64 \
     && luarocks install lua-protobuf \
