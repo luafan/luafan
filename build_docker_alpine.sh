20c9cceb585cb1098442fa96d52f0096364df4a6
Sam Chang <sam.chang@me.com>
2026-05-15 00:29:06 +0800

    Enable parallel compilation for MariaDB and OpenSSL in Docker builds

--- build_docker_alpine.sh
+++ build_docker_alpine.sh
@@ -10,8 +10,8 @@ apk add --update bsd-compat-headers tzdata linux-headers git lua5.3-dev lua5.3 l
     && ln -s /usr/bin/lua5.3 /usr/bin/lua \
     && wget http://luarocks.org/releases/luarocks-$LUAROCKS_VERSION.tar.gz && tar xzf luarocks-$LUAROCKS_VERSION.tar.gz && cd luarocks-$LUAROCKS_VERSION && ./configure && make build && make install && cd .. \
     && wget https://github.com/MariaDB/server/archive/mariadb-$MARIADB_VERSION.tar.gz && tar xzf mariadb-$MARIADB_VERSION.tar.gz \
-    && cd server-mariadb-$MARIADB_VERSION && sed -i '/HAVE_UCONTEXT_H/d' config.h.cmake && cmake -DWITHOUT_TOKUDB=1 . && cd libmysql && make install && cd ../include && make install && cd ../.. && rm -rf mariadb-$MARIADB_VERSION.tar.gz server-mariadb-$MARIADB_VERSION \
-    && wget https://www.openssl.org/source/openssl-$OPENSSL_VERSION.tar.gz && tar xzf openssl-$OPENSSL_VERSION.tar.gz && cd openssl-$OPENSSL_VERSION && ./config && make && make install && cd .. && rm -rf openssl* \
+    && cd server-mariadb-$MARIADB_VERSION && sed -i '/HAVE_UCONTEXT_H/d' config.h.cmake && cmake -DWITHOUT_TOKUDB=1 . && cd libmysql && make -j$(nproc) install && cd ../include && make install && cd ../.. && rm -rf mariadb-$MARIADB_VERSION.tar.gz server-mariadb-$MARIADB_VERSION \
+    && wget https://www.openssl.org/source/openssl-$OPENSSL_VERSION.tar.gz && tar xzf openssl-$OPENSSL_VERSION.tar.gz && cd openssl-$OPENSSL_VERSION && ./config && make -j$(nproc) && make install && cd .. && rm -rf openssl* \
     && git clone https://github.com/luafan/luafan.git && cd luafan && luarocks make luafan-$LUAFAN_VERSION.rockspec MARIADB_DIR=/usr/local/mysql && cd .. && rm -rf luafan \
     && luarocks install compat53 && luarocks install lpeg && luarocks install lua-cjson 2.1.0-1 && luarocks install luafilesystem \
     && luarocks install lzlib ZLIB_LIBDIR=/lib && luarocks install openssl && luarocks install lbase64 \
