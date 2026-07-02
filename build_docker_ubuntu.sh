c4259d0c70ef463e4863655ebdc56129626cc5eb
Sam Chang <sam.chang@me.com>
2026-07-02 19:15:07 +0800

    Replace curl with curl-impersonate Chrome variant
    Build curl with Chrome TLS fingerprint patches from lwthiker/curl-impersonate.
    Adds nghttp2 and brotli as dependencies for full HTTP/2 and compression support.

--- build_docker_ubuntu.sh
+++ build_docker_ubuntu.sh
@@ -6,24 +6,42 @@ LUAROCKS_VERSION=3.13.0
 MARIADB_VERSION=5.5.68
 BORINGSSL_COMMIT=1b7fdbd9101dedc3e0aa3fcf4ff74eacddb34ecc
 LIBEVENT_VERSION=2.1.12-stable
-CURL_VERSION=8.7.1
+CURL_IMPERSONATE_VERSION=0.6.1
+BROTLI_VERSION=1.0.9
+NGHTTP2_VERSION=nghttp2-1.56.0
 
 apt update \
-    && apt install -y libsqlite3-0 libsqlite3-dev tzdata wget liblua5.3-dev lua5.3 unzip zlib1g-dev make gcc libc-dev git cmake g++ bison libncurses5-dev golang-go ca-certificates \
-    && git clone https://boringssl.googlesource.com/boringssl && cd boringssl && git checkout $BORINGSSL_COMMIT \
-    && mkdir build && cd build && cmake -DCMAKE_POSITION_INDEPENDENT_CODE=ON .. && make -j$(nproc) && cd .. \
-    && cp -r include/openssl /usr/local/include/ && cp build/ssl/libssl.a /usr/local/lib/ && cp build/crypto/libcrypto.a /usr/local/lib/ \
+    && apt install -y libsqlite3-0 libsqlite3-dev tzdata wget liblua5.3-dev lua5.3 unzip zlib1g-dev make gcc libc-dev git cmake g++ bison libncurses5-dev golang-go ca-certificates ninja-build autoconf automake libtool pkg-config \
+    && git clone https://github.com/lwthiker/curl-impersonate.git && cd curl-impersonate \
+    && curl -L https://github.com/google/boringssl/archive/$BORINGSSL_COMMIT.zip -o boringssl.zip \
+    && unzip -q boringssl.zip && mv boringssl-$BORINGSSL_COMMIT boringssl \
+    && cd boringssl && for p in ../chrome/patches/boringssl-*.patch; do patch -p1 < $p; done \
+    && mkdir build && cd build \
+    && cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_C_FLAGS="-Wno-unknown-warning-option -Wno-stringop-overflow -Wno-array-bounds" -GNinja .. \
+    && ninja \
+    && mkdir -p lib && ln -sf ../crypto/libcrypto.a lib/libcrypto.a && ln -sf ../ssl/libssl.a lib/libssl.a && cp -Rf ../include . \
+    && cd ../.. \
+    && cp -r boringssl/include/openssl /usr/local/include/ \
+    && cp boringssl/build/ssl/libssl.a /usr/local/lib/ && cp boringssl/build/crypto/libcrypto.a /usr/local/lib/ \
     && echo '#ifndef SHLIB_VERSION_NUMBER' >> /usr/local/include/openssl/opensslv.h \
     && echo '#define SHLIB_VERSION_NUMBER "BoringSSL"' >> /usr/local/include/openssl/opensslv.h \
     && echo '#endif' >> /usr/local/include/openssl/opensslv.h \
-    && cd .. && rm -rf boringssl \
+    && wget https://github.com/nghttp2/nghttp2/releases/download/v1.56.0/$NGHTTP2_VERSION.tar.bz2 \
+    && tar xjf $NGHTTP2_VERSION.tar.bz2 && cd $NGHTTP2_VERSION \
+    && ./configure --prefix=/usr/local --with-pic --enable-lib-only --disable-shared --disable-python-bindings \
+    && make -j$(nproc) && make install && cd .. \
+    && wget https://github.com/google/brotli/archive/refs/tags/v$BROTLI_VERSION.tar.gz -O brotli-$BROTLI_VERSION.tar.gz \
+    && tar xzf brotli-$BROTLI_VERSION.tar.gz && cd brotli-$BROTLI_VERSION && mkdir out && cd out \
+    && cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_INSTALL_LIBDIR=lib .. \
+    && cmake --build . --config Release --target install && cd ../.. \
+    && wget https://curl.se/download/curl-8.1.1.tar.xz && tar xf curl-8.1.1.tar.xz && cd curl-8.1.1 \
+    && for p in ../chrome/patches/curl-*.patch; do patch -p1 < $p; done && autoreconf -fi \
+    && ./configure --prefix=/usr/local --with-nghttp2=/usr/local --with-brotli=/usr/local --with-openssl=/root/curl-impersonate/boringssl/build --with-zlib --enable-websockets USE_CURL_SSLKEYLOGFILE=true LIBS="-lpthread" \
+    && make -j$(nproc) && make install && ldconfig && cd ../.. && rm -rf curl-impersonate \
     && wget https://github.com/libevent/libevent/releases/download/release-$LIBEVENT_VERSION/libevent-$LIBEVENT_VERSION.tar.gz && tar xzf libevent-$LIBEVENT_VERSION.tar.gz \
     && cd libevent-$LIBEVENT_VERSION && mkdir build && cd build \
     && cmake -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DEVENT__LIBRARY_TYPE=BOTH -DOPENSSL_ROOT_DIR=/usr/local -DOPENSSL_INCLUDE_DIR=/usr/local/include -DOPENSSL_CRYPTO_LIBRARY=/usr/local/lib/libcrypto.a -DOPENSSL_SSL_LIBRARY=/usr/local/lib/libssl.a .. \
     && make -j$(nproc) && make install && cd ../.. && rm -rf libevent-$LIBEVENT_VERSION* \
-    && wget https://curl.se/download/curl-$CURL_VERSION.tar.gz && tar xzf curl-$CURL_VERSION.tar.gz \
-    && cd curl-$CURL_VERSION && ./configure --with-openssl=/usr/local --without-libpsl LDFLAGS="-L/usr/local/lib" CPPFLAGS="-I/usr/local/include" LIBS="-lpthread" \
-    && make -j$(nproc) && make install && ldconfig && cd .. && rm -rf curl-$CURL_VERSION* \
     && wget https://github.com/MariaDB/server/archive/mariadb-$MARIADB_VERSION.tar.gz && tar xzf mariadb-$MARIADB_VERSION.tar.gz && cd server-mariadb-$MARIADB_VERSION && cmake . && cd libmysql && make -j$(nproc) install && cd ../include && make install && cd ../.. && rm -rf mariadb-$MARIADB_VERSION.tar.gz server-mariadb-$MARIADB_VERSION \
     && wget https://luarocks.github.io/luarocks/releases/luarocks-$LUAROCKS_VERSION.tar.gz && tar xzf luarocks-$LUAROCKS_VERSION.tar.gz && cd luarocks-$LUAROCKS_VERSION && ./configure && make build && make install && cd .. \
     && git clone https://github.com/luafan/luafan.git && cd luafan && luarocks make luafan-$LUAFAN_VERSION.rockspec MARIADB_DIR=/usr/local/mysql CURL_INCDIR=/usr/local/include && cd .. && rm -rf luafan \
@@ -33,7 +51,7 @@ apt update \
     && luarocks install lmd5 \
     && luarocks install lua-iconv \
     && luarocks install lsqlite3 \
-    && cd /root && cd luarocks-$LUAROCKS_VERSION && make uninstall && cd .. && rm -rf luarocks* \
-    && apt-get -y remove g++ bison libncurses5-dev liblua5.3-dev libc-dev zlib1g-dev unzip cmake make gcc binutils libc-dev-bin git golang-go \
+    && cd luarocks-$LUAROCKS_VERSION && make uninstall && cd .. && rm -rf luarocks* \
+    && apt-get -y remove g++ bison libncurses5-dev liblua5.3-dev libc-dev zlib1g-dev unzip cmake make gcc binutils libc-dev-bin git golang-go ninja-build autoconf automake libtool pkg-config \
     && apt-get -y autoremove \
     && rm -rf /usr/local/share/doc /usr/local/mysql/lib/*.a /usr/local/mysql/include /usr/local/bin /var/lib/apt/lists/* || exit -1
